b73097964ad8ef7f25b0dfbb316b02dc45a1c73a
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-10-26 17:56:51 +0100

    delete and check btn

--- main.js
+++ main.js
@@ -2,32 +2,40 @@ const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const ul = document.getElementById("ul");
 
-/* btn.addEventListener("click", () => {
-  if (input.value === "") {
-    null;
-  } else {
-    let text = document.createElement("li");
-    text.innerHTML = `${input.value} <i class="fa fa-trash"></i><i class="fa fa-check"></i>`;
-    ul.appendChild(text);
-    input.value = "";
-    let trash = document.querySelector(".fa.fa-trash");
-    text.addEventListener("click", () => {
-      console.log(trash);
-    });
-  }
-}); */
-
 btn.addEventListener("click", addTodo);
+ul.addEventListener("click", deleteCheck);
 
 function addTodo(e) {
   e.preventDefault();
-  const newTodo = document.createElement("li");
-  newTodo.innerText = "yo";
-  ul.appendChild(newTodo);
-  const completeBtn = document.createElement("button");
-  completeBtn.innerHTML = '<i class="fa fa-check"></i>';
-  ul.appendChild(completeBtn);
-  const trashBtn = document.createElement("button");
-  trashBtn.innerHTML = '<i class="fa fa-trash"></i>';
-  ul.appendChild(trashBtn);
+  if (input.value === "") {
+    null;
+  } else {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = input.value;
+    todoDiv.appendChild(newTodo);
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("checked");
+    completeBtn.innerHTML = '<i class="fa fa-check"></i>';
+    todoDiv.appendChild(completeBtn);
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trash");
+    trashBtn.innerHTML = '<i class="fa fa-trash"></i>';
+    todoDiv.appendChild(trashBtn);
+    ul.appendChild(todoDiv);
+    input.value = "";
+  }
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  if (item.classList.contains("trash")) {
+    const remove = item.parentElement;
+    remove.remove();
+  }
+  if (item.classList.contains("checked")) {
+    const checked = item.parentElement;
+    checked.classList.toggle("line-through");
+  }
 }
