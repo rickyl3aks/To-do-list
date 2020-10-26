c92a72e40bb860553e7113af5c7db94b2345059c
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-10-26 15:54:43 +0100

    to do list

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
-//close button
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const ul = document.getElementById("ul");
 
-var closeButton = document.getElementsByClassName('close');
-
-
-
-//creating todo function
-
-function createNewElement() {
-  var li = document.createElement("li");
-  var theInputValue = document.getElementById("the-input").nodeValue;
-  var textNode = document.createTextNode(theInputValue);
-  li.appendChild(textNode);
-
-  if (theInputValue === "") {
-    alert("This cannot be empty");
+/* btn.addEventListener("click", () => {
+  if (input.value === "") {
+    null;
   } else {
-    document.getElementById("the-ul").appendChild(li);
+    let text = document.createElement("li");
+    text.innerHTML = `${input.value} <i class="fa fa-trash"></i><i class="fa fa-check"></i>`;
+    ul.appendChild(text);
+    input.value = "";
+    let trash = document.querySelector(".fa.fa-trash");
+    text.addEventListener("click", () => {
+      console.log(trash);
+    });
   }
+}); */
 
-  document.getElementById("the-input").value = "";
-  var thePanTag = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  thePanTag.className = "close";
-  thePanTag.appendChild(txt);
-  li.appendChild(thePanTag);
+btn.addEventListener("click", addTodo);
 
-  //removing items when click on SPAN CLOSE BUTTON
-
-  for(i = 0, i < closeButton.length, i++){
-      closeButton[i].onclick = function(){
-          var theDiv = this.parentElement;
-          theDiv.style.display = 'none';
-      }
-  }
+function addTodo(e) {
+  e.preventDefault();
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "yo";
+  ul.appendChild(newTodo);
+  const completeBtn = document.createElement("button");
+  completeBtn.innerHTML = '<i class="fa fa-check"></i>';
+  ul.appendChild(completeBtn);
+  const trashBtn = document.createElement("button");
+  trashBtn.innerHTML = '<i class="fa fa-trash"></i>';
+  ul.appendChild(trashBtn);
 }
-
-
